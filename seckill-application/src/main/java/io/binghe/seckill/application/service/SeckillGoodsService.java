45f99ec23145f1692587febe8ca1e7bfb60eb20f
gaoshou45 <477591620@qq.com>
2026-02-12 22:33:51 +0800

    feat: 秒杀商品后端业务与接口开发

---
+++ seckill-application/src/main/java/io/binghe/seckill/application/service/SeckillGoodsService.java
@@ -0,0 +1,38 @@
+package io.binghe.seckill.application.service;
+
+import io.binghe.seckill.domain.dto.SeckillGoodsDTO;
+import io.binghe.seckill.domain.model.SeckillGoods;
+
+import java.util.List;
+
+public interface SeckillGoodsService {
+    /**
+     * 保存商品信息
+     */
+    int saveSeckillGoods(SeckillGoodsDTO seckillGoodsDTO);
+
+    /**
+     * 根据id获取商品详细信息
+     */
+    SeckillGoods getSeckillGoodsId(Long id);
+
+    /**
+     * 根据活动id获取商品列表
+     */
+    List<SeckillGoods> getSeckillGoodsByActivityId(Long activityId);
+
+    /**
+     * 修改商品状态
+     */
+    int updateStatus(Integer status, Long id);
+
+    /**
+     * 扣减库存
+     */
+    int updateAvailableStock(Integer count, Long id);
+
+    /**
+     * 获取当前可用库存
+     */
+    Integer getAvailableStockById(Long id);
+}
---
+++ seckill-application/src/main/java/io/binghe/seckill/application/service/impl/SeckillGoodsServiceImpl.java
@@ -0,0 +1,73 @@
+package io.binghe.seckill.application.service.impl;
+
+import io.binghe.seckill.application.service.SeckillGoodsService;
+import io.binghe.seckill.domain.code.HttpCode;
+import io.binghe.seckill.domain.dto.SeckillGoodsDTO;
+import io.binghe.seckill.domain.enums.SeckillGoodsStatus;
+import io.binghe.seckill.domain.exception.SeckillException;
+import io.binghe.seckill.domain.model.SeckillActivity;
+import io.binghe.seckill.domain.model.SeckillGoods;
+import io.binghe.seckill.domain.repository.SeckillActivityRepository;
+import io.binghe.seckill.domain.repository.SeckillGoodsRepository;
+import io.binghe.seckill.infrastructure.utils.beans.BeanUtil;
+import io.binghe.seckill.infrastructure.utils.id.SnowFlakeFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class SeckillGoodsServiceImpl implements SeckillGoodsService {
+    @Autowired
+    private SeckillGoodsRepository seckillGoodsRepository;
+    @Autowired
+    private SeckillActivityRepository seckillActivityRepository;
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public int saveSeckillGoods(SeckillGoodsDTO seckillGoodsDTO) {
+        if (seckillGoodsDTO == null){
+            throw new SeckillException(HttpCode.PARAMS_INVALID);
+        }
+        SeckillActivity seckillActivity = seckillActivityRepository.getSeckillActivityById(seckillGoodsDTO.getActivityId());
+        if (seckillActivity == null){
+            throw new SeckillException(HttpCode.ACTIVITY_NOT_EXISTS);
+        }
+        SeckillGoods seckillGoods = new SeckillGoods();
+        BeanUtil.copyProperties(seckillGoodsDTO, seckillGoods);
+        seckillGoods.setStartTime(seckillActivity.getStartTime());
+        seckillGoods.setEndTime(seckillActivity.getEndTime());
+        seckillGoods.setAvailableStock(seckillGoodsDTO.getInitialStock());
+        seckillGoods.setId(SnowFlakeFactory.getSnowFlakeFromCache().nextId());
+        seckillGoods.setStatus(SeckillGoodsStatus.PUBLISHED.getCode());
+        return seckillGoodsRepository.saveSeckillGoods(seckillGoods);
+    }
+
+    @Override
+    public SeckillGoods getSeckillGoodsId(Long id) {
+        return seckillGoodsRepository.getSeckillGoodsId(id);
+    }
+
+
+    @Override
+    public List<SeckillGoods> getSeckillGoodsByActivityId(Long activityId) {
+        return seckillGoodsRepository.getSeckillGoodsByActivityId(activityId);
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public int updateStatus(Integer status, Long id) {
+        return seckillGoodsRepository.updateStatus(status, id);
+    }
+
+    @Override
+    public int updateAvailableStock(Integer count, Long id) {
+        return seckillGoodsRepository.updateAvailableStock(count, id);
+    }
+
+    @Override
+    public Integer getAvailableStockById(Long id) {
+        return seckillGoodsRepository.getAvailableStockById(id);
+    }
+}
