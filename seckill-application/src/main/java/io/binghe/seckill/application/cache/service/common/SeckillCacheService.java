3df1dd6e01e971cd4c4f66f367064b64fed995f5
gaoshou45 <477591620@qq.com>
2026-03-09 18:03:18 +0800

    引入Dubbo和Nacos重构整个项目

--- seckill-application/src/main/java/io/binghe/seckill/application/cache/service/common/SeckillCacheService.java
+++
@@ -1,9 +0,0 @@
-package io.binghe.seckill.application.cache.service.common;
-
-/**
- * 通用缓存接口
- */
-public interface SeckillCacheService {
-
-    String buildCacheKey(Object key);
-}
--- seckill-domain/src/main/java/io/binghe/seckill/domain/event/SeckillOrderEvent.java
+++
@@ -1,8 +0,0 @@
-package io.binghe.seckill.domain.event;
-
-public class SeckillOrderEvent extends SeckillBaseEvent {
-
-    public SeckillOrderEvent(Long id, Integer status) {
-        super(id, status);
-    }
-}
